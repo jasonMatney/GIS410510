b934d94ba5a66fea35a3cb05c65bf837e3877d01
Jason Matney <jamatney@gmail.com>
2016-10-25 19:43:00 -0400

    Create OrdinaryKriging.R

---
+++ OrdinaryKriging.R
@@ -0,0 +1,73 @@
+####################################################################################
+################[ Ordinary Kriging with Jockey's Ridge LiDAR data  ]################
+################[ NC State University GIS 410/510 - Fall 16        ]################
+################[ 10/25/2016                                       ]################
+####################################################################################
+
+# Inspiration and most of the code from: 
+# https://rpubs.com/nabilabd/118172
+# by Nabil A.
+
+library(sp)
+library(gstat)
+# packages for manipulation & visualization
+suppressPackageStartupMessages({
+  library(____BLANK_1____) # for "glimpse" and "sample_n"
+  library(ggplot2)
+  library(scales) # for "comma"
+  library(magrittr)
+})
+
+library(RCurl)
+# Load the data
+JR_elev_2001_30K <- ____BLANK_2____("https://raw.githubusercontent.com/jasonMatney/GIS410510/master/JR_elev_2001_30K.csv")
+JR_elev_2001 <- read.csv(text = JR_elev_2001_30K, col.names=c("x","y","z","id"))[ ,1:3]
+JR_elev_2001 <- sample_n(JR_elev_2001, 1000) # downsample
+
+# Inspect the data
+glimpse(JR_elev_2001)
+
+# Make a spatial object
+coordinates(JR_elev_2001) <- ~x+y
+
+# Fit variogram 
+lzn.vgm <- variogram(z~1, JR_elev_2001) # calculates sample variogram values 
+lzn.fit <- fit.variogram(lzn.vgm, model=vgm(psill=35, model="Exp", range=5)) # fit model
+
+# Plot the sample values, along with the fit model
+plot(lzn.vgm, lzn.fit) 
+
+# Sample spatial domain to interpolate over
+JR_elev_2001_grid = spsample(JR_elev_2001, type = "regular", cellsize = ____BLANK_3____) 
+gridded(JR_elev_2001_grid) = TRUE
+
+# Create plot of the downscaled data
+plot1 <- JR_elev_2001 %>% ____BLANK_4____ %>%
+  ggplot(aes(x, y)) + geom_point(size=1) + coord_equal() + 
+  ggtitle("Points with measurements")
+
+# Create plot of gridded area over the region of interest
+plot2 <- JR_elev_2001_grid %>% as.data.frame %>%
+  ggplot(aes(x1, x2)) + geom_point(size=1) + coord_equal() + 
+  ggtitle("Points at which to estimate")
+
+# Display those plots side-by-side
+library(gridExtra)
+grid.arrange(plot1, plot2, ncol = 2)
+
+# Time the kriging operation
+# Start the clock!
+ptm <- proc.time()
+
+# Perform Ordinary Kriging 
+lzn.kriged <- ____BLANK_5____(z ~ 1, JR_elev_2001, JR_elev_2001_grid, model=lzn.fit)
+
+# Stop the clock!
+proc.time() - ptm
+
+# Plot using ggplot2
+p <- lzn.kriged %>% as.data.frame %>%
+  ggplot(aes(x1, x2))
+
+p + geom_point(aes(colour = var1.pred))  + ____BLANK_6____(low = "yellow", high="red") + coord_equal() 
+
